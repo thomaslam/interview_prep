57288302bad7c43cb265bbb5d0b6322c6bac0845
Thomas Lam <tuananhlam95@gmail.com>
2018-11-14 15:07:48 -0500

    second sol to palindrome list

--- CTCI/2-linked-lists/PalindromeList.java
+++ CTCI/2-linked-lists/PalindromeList.java
@@ -22,4 +22,41 @@ public class PalindromeList {
 		}
 		return true;
 	}
+
+	// Second solution using no extra storage
+	public boolean isPalindrome(Node l) {
+		Node fast = l;
+		Node slow = l;
+
+		while (fast != null && fast.next != null) {
+			fast = fast.next.next;
+			slow = slow.next;
+		}
+
+		if (fast != null) {
+			slow = slow.next;
+		}
+		slow = reverse(slow);
+		fast = head;
+
+		while (slow != null) {
+			if (fast.val != slow.val) {
+				return false;
+			}
+			fast = fast.next;
+			slow = slow.next;
+		}
+		return true;
+	}
+
+	public Node reverse(Node head) {
+		Node prev = null;
+		while (head != null) {
+			Node next = head.next;
+			head.next = prev;
+			prev = head;
+			head = next;
+		}
+		return prev;
+	}
 }
